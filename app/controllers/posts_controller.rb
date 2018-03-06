83fb76f087e7d0c09875cf1052043df33f112624
jiaowagn <wangjiaohere@gmail.com>
2018-03-06 16:41:32 +0800

    edit and delete post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new 
+    @post = Post.new
   end
 
   def create
@@ -18,6 +18,25 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to root_path
   end
 
   private
